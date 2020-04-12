c0f110794160c7206da745debc024d13a80bf04e
CodeItQuick <evanontario@hotmail.com>
2020-04-12 20:09:06 -0300

    improvement(getting-started.js): there are now media queries
    The getting started section is more mobile responsive.

--- src/common/slanted-borders-helpers.js
+++ src/common/slanted-borders-helpers.js
@@ -3,4 +3,6 @@ import styled from "styled-components"
 // This is the main thing to use for the home page (and probably other pages)
 export const SlantedBorderElement = styled.div`
   /* Your styling goes here */
+  clip-path: polygon(0 ${({offsetAmount = 4}) => offsetAmount + '%'}, 100% 0, 100% ${({offsetAmountBottom = 96}) => offsetAmountBottom + '%'}, 0 100%);
+  transform: translateY(${({translateY = '-10%'}) => translateY});
 `
--- src/components/getting-started.js
+++ src/components/getting-started.js
@@ -4,57 +4,102 @@ import { Layout } from "../components/layout";
 import SEO from "../components/seo";
 import styled from 'styled-components';
 import colors from "../common/colors";
+import { SlantedBorderElement } from "../common/slanted-borders-helpers.js";
 
-const MainContainer = styled.div`
+import breakpoints
+from '../common/breakpoints.js';
+
+const {
+  mobile,
+  tablet,
+  medium,
+  large,
+  xl,
+  xxl,
+} = breakpoints;
+const MainContainer = styled(SlantedBorderElement)`
   color: ${colors.typography.light};
   background-color: ${colors.backgrounds.dark1};
-  clip-path: polygon(0 4%, 100% 0, 100% 96%, 0 100%);
-  transform: translateY(-10%);
   `;
 const Title = styled.h2`
   padding-top: 32px;
   text-align: center;
   font-weight: 600;
-  font-size: 36px;
+  font-size: 20px;
   line-height: 47px;
+  
+  @media (min-width: 480px) {
+    font-size: 36px;
+  }
 `;
 const Section = styled.div`
   display: flex;
   min-height: 200px;
   justify-content: center;
   align-items: center;
-  margin-left: -20%;
-  min-width: 400px;
+  min-width: 100px;
+  transform: translateX(5%);
+  @media (min-width: 480px) and (max-width: 760px) {
+    min-width: 150px;
+  }
+  @media (min-width: 760px) {
+    transform: translateX(-15%);
+    min-width: 200px;
+  }
 `;
 const RightAlignSection = styled(Section)`
-  margin-right: -40%;
+  @media (min-width: 480px)  {
+    min-width: 150px;
+  }
+  @media (min-width: 760px)  {
+    transform: translateX(15%);
+    min-width: 300px;
+  }
 `;
 const BigNumber = styled.div`
   font-size: 60px;
   align-items: center;
   justify-content: flex-start;
-  width: 100px;
+  width: 50px;
   font-weight: 600;
-  font-size: 144px;
+  font-size: 48px;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+  @media (min-width: 480px) {
+    font-size: 144px;
+    width: 100px;
+  }
 `;
 const Description = styled.div`
   flex-wrap: wrap;
   justify-content: flex-start;
-  width: 420px;
+  width: 300px;
+  @media (min-width: 480px)  {
+    min-width: 150px;
+  }
+  @media (min-width: 760px)  {
+    transform: translateX(15%);
+    min-width: 300px;
+    width: 380px;
+  }
 `;
 const SectionTitle = styled.h4`
   margin: 0;
   font-weight: 600;
-  font-size: 36px;
+  font-size: 24px;
   line-height: 47px;
+  @media (min-width: 480px) {
+    font-size: 36px;
+  }
 `;
 const SectionParagraph = styled.p`
   margin: 0;
   font-size: 12px;
   font-weight: 600;
-  font-size: 24px;
   line-height: 31px;
+  @media (min-width: 480px) {
+    font-size: 24px;
+  }
 `;
 
 const GettingStartedComponent = () => (
