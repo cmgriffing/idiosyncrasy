2a5b3606ecc1257badb943735f55c8d4ca1c62cb
CodeItQuick <evanontario@hotmail.com>
2020-04-26 21:37:38 -0300

    improvement(slanted-borders-helper): prettiered slanted borders
    Prettiered the file

--- src/common/slanted-borders-helpers.js
+++ src/common/slanted-borders-helpers.js
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 // This is the main thing to use for the home page (and probably other pages)
 export const SlantedBorderElement = styled.div`
   /* Your styling goes here */
 
-  clip-path: polygon(0 20px, 100% 0,100% 100%, 0 100%);
+  clip-path: polygon(0 20px, 100% 0, 100% 100%, 0 100%);
   margin-top: -50px;
   padding: 10px;
 `
