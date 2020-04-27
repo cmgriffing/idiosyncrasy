3bc6fdd4e1591a574a8a269305e7ce5c44bc79ac
CodeItQuick <evanontario@hotmail.com>
2020-04-26 21:36:58 -0300

    improvement(getting-started.js): prettified it
    Prettiered getting started

--- src/components/getting-started.js
+++ src/components/getting-started.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react"
 
-import { Layout } from "../components/layout";
-import SEO from "../components/seo";
-import styled from 'styled-components';
-import colors from "../common/colors";
-import { SlantedBorderElement } from "../common/slanted-borders-helpers.js";
+import { Layout } from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+import colors from "../common/colors"
+import { SlantedBorderElement } from "../common/slanted-borders-helpers.js"
 
-import breakpoints from '../common/breakpoints.js';
+import breakpoints from "../common/breakpoints.js"
 
 const MainContainer = styled(SlantedBorderElement)`
   color: ${colors.typography.light};
   background-color: ${colors.backgrounds.dark1};
-  `;
+`
 const Title = styled.h2`
   padding-top: 32px;
   text-align: center;
   font-weight: 600;
   font-size: 20px;
   line-height: 47px;
-  
+
   @media (min-width: ${breakpoints.mobile}px) {
     font-size: 36px;
   }
-`;
+`
 const Section = styled.div`
   display: flex;
   min-height: 200px;
@@ -37,16 +37,16 @@ const Section = styled.div`
     transform: translateX(-15%);
     min-width: 200px;
   }
-`;
+`
 const RightAlignSection = styled(Section)`
-  @media (min-width: ${breakpoints.mobile}px)  {
+  @media (min-width: ${breakpoints.mobile}px) {
     min-width: 150px;
   }
-  @media (min-width: ${breakpoints.tablet}px)  {
+  @media (min-width: ${breakpoints.tablet}px) {
     transform: translateX(15%);
     min-width: 300px;
   }
-`;
+`
 const BigNumber = styled.div`
   font-size: 60px;
   align-items: center;
@@ -60,20 +60,20 @@ const BigNumber = styled.div`
     font-size: 144px;
     width: 100px;
   }
-`;
+`
 const Description = styled.div`
   flex-wrap: wrap;
   justify-content: flex-start;
   width: 300px;
-  @media (min-width: ${breakpoints.mobile}px)  {
+  @media (min-width: ${breakpoints.mobile}px) {
     min-width: 150px;
   }
-  @media (min-width: ${breakpoints.tablet}px)  {
+  @media (min-width: ${breakpoints.tablet}px) {
     transform: translateX(15%);
     min-width: 300px;
     width: 380px;
   }
-`;
+`
 const SectionTitle = styled.h4`
   margin: 0;
   font-weight: 600;
@@ -82,7 +82,7 @@ const SectionTitle = styled.h4`
   @media (min-width: ${breakpoints.mobile}px) {
     font-size: 36px;
   }
-`;
+`
 const SectionParagraph = styled.p`
   margin: 0;
   font-size: 12px;
@@ -91,38 +91,38 @@ const SectionParagraph = styled.p`
   @media (min-width: ${breakpoints.mobile}px) {
     font-size: 24px;
   }
-`;
+`
 
 const GettingStartedComponent = () => (
-    <MainContainer>
+  <MainContainer>
+    <Title>Getting Started</Title>
 
-        <Title>Getting Started</Title>
+    <Section>
+      <BigNumber>1</BigNumber>
+      <Description>
+        <SectionTitle>Sign up and log in.</SectionTitle>
+        <SectionParagraph>It takes 30 seconds</SectionParagraph>
+      </Description>
+    </Section>
 
-        <Section>
-            <BigNumber>1</BigNumber>
-            <Description>
-                <SectionTitle>Sign up and log in.</SectionTitle> 
-                <SectionParagraph>It takes 30 seconds</SectionParagraph>
-            </Description>
-        </Section>
+    <RightAlignSection>
+      <BigNumber>2</BigNumber>
+      <Description>
+        <SectionTitle>Configure some settings. </SectionTitle>
+        <SectionParagraph>
+          Add some Alerts. Connect up some lights.
+        </SectionParagraph>
+      </Description>
+    </RightAlignSection>
 
-        <RightAlignSection >
-            <BigNumber>2</BigNumber>
-            <Description>
-                <SectionTitle>Configure some settings. </SectionTitle> 
-                <SectionParagraph>Add some Alerts. Connect up some lights.</SectionParagraph>
-            </Description>
-        </RightAlignSection> 
+    <Section>
+      <BigNumber>3</BigNumber>
+      <Description>
+        <SectionTitle>Profit!</SectionTitle>
+        <SectionParagraph>The Sky is the limit.</SectionParagraph>
+      </Description>
+    </Section>
+  </MainContainer>
+)
 
-        <Section>
-            <BigNumber>3</BigNumber>
-            <Description>
-                <SectionTitle>Profit!</SectionTitle> 
-                <SectionParagraph>The Sky is the limit.</SectionParagraph>
-            </Description>
-        </Section>
-
-    </MainContainer>
-);
-
-export default GettingStartedComponent;
+export default GettingStartedComponent
