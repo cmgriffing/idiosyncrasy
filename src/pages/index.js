013a2e42dfd9410c4dd7a28cca5f81f888311fe3
CodeItQuick <evanontario@hotmail.com>
2020-12-26 14:36:35 -0400

    fix(index.js): removed Extra Image reference

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import { Layout } from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import GettingStartedComponent from "../components/getting-started"
 
